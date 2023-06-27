eeea03cc3fdab9a2a4fab86884165204eb2c829b
Rafael Felipe <rafael.felipe1989@gmail.com>
2023-06-27 09:05:51 -0300

    Initial Commit

---
+++ EasyLocale/EasyLocaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyLocaleApp.swift
+//  EasyLocale
+//
+//  Created by Rafael Ramos on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EasyLocaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
