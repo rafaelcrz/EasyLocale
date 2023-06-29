9aae0fa3fc8b7343be7a2d57290760efd5b3afce
Rafael Felipe <rafael.felipe1989@gmail.com>
2023-06-29 19:24:20 -0300

    add custom strings extension

---
+++ EasyLocale/UTType+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UTType+Extensions.swift
+//  EasyLocale
+//
+//  Created by Rafael Ramos on 29/06/23.
+//
+
+import Foundation
+import UniformTypeIdentifiers
+
+extension UTType {
+    static let strings = UTType(exportedAs: "strings", conformingTo: .text)
+}
