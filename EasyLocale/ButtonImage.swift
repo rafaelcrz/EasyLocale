144e237d45d6f31a99f89aa9781eaecbcbfbc535
Rafael Felipe <rafael.felipe1989@gmail.com>
2023-06-29 21:11:21 -0300

    Update ButtonImage.swift

--- EasyLocale/ButtonImage.swift
+++ EasyLocale/ButtonImage.swift
@@ -9,29 +9,30 @@ import Foundation
 import SwiftUI
 
 struct ButtonImage: View {
-    let systemName: String
+    let systemName: Constants.SystemImage
     var text: String?
     var foregroundColor: Color = Color(.labelColor)
-    var action: () -> Void
+    var controlSize: ControlSize = .regular
+    var action: () -> ()
     
     var body: some View {
         Button {
             action()
         } label: {
             HStack {
+                Image(systemName: systemName.rawValue)
+                    .foregroundColor(foregroundColor)
+                    .padding(4)
                 if let text = text {
                     Text(text)
                 }
-                Image(systemName: systemName)
-                    .foregroundColor(foregroundColor)
-                    .padding(4)
             }
-        }
+        }.controlSize(controlSize)
     }
 }
 
 struct ButtonImage_Previews: PreviewProvider {
     static var previews: some View {
-        ButtonImage(systemName: "trash", action: {})
+        ButtonImage(systemName: .trash, action: {})
     }
 }
